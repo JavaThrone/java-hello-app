7d680954190ab496a8afcf4cefef1712706f4561
azima <for789456123FUN!Q@>
2021-08-16 22:45:06 +0300

    Initial commit

---
+++ src/main/java/com/example/javahelloapp/JavaHelloAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahelloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javahelloapp/JavaHelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javahelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
